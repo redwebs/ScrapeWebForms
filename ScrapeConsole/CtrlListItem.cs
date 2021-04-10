776b19cdd836d94df167d262fb5462f4523e3000
fredwebs <fredwebs@gmail.com>
2021-04-10 13:24:26 -0400

    Added CtrlListItem class.

---
+++ ScrapeConsole/CtrlListItem.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ScrapeConsole
+{
+    public class CtrlListItem
+    {
+        protected string Text;
+        protected int ItemDataVal;
+
+        public CtrlListItem(string text)
+        {
+            Text = text;
+            ItemDataVal = 0;
+        }
+
+        public CtrlListItem(string text, int itemData)
+        {
+            Text = text;
+            ItemDataVal = itemData;
+        }
+
+        public CtrlListItem(string text, string itemDataString)
+        {
+            Text = text;
+            int.TryParse(itemDataString, out ItemDataVal);
+        }
+
+        public int ItemData
+        {
+            get => ItemDataVal;
+            set => ItemDataVal = value;
+        }
+
+        public string ItemDataString
+        {
+            get => ItemData.ToString();
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
